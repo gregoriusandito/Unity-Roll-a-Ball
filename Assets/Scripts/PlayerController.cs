3c4d2f09d1a455dc404cd8702f0f833163b47f37
gregoriusandito <gregorius.andito@gmail.com>
2017-12-28 19:31:22 +0700

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+
+    //UI elements
+    public Text countText;
+    public Text winText;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    //custom functions to avoid calling some instance twice
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10) {
+            winText.text = "You Win!";
+        }
+    }
+
+}
